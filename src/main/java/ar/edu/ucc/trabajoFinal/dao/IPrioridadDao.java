7d2d3a5fe81d8942f6f1b47abb446446e3835a38
Ezequiel <bergeroezequiel@gmail.com>
2018-04-10 19:40:58 -0300

    Creacion del modelo de Criticidad con su respectivo DAO generico.

---
+++ src/main/java/ar/edu/ucc/trabajoFinal/dao/IPrioridadDao.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ar.edu.ucc.trabajoFinal.dao;
+
+import ar.edu.ucc.trabajoFinal.model.Prioridad;
+
+/**
+ *
+ * @author ezequiel
+ */
+public interface IPrioridadDao extends DaoGenerico<Prioridad, Long>{
+    
+}
---
+++ src/main/java/ar/edu/ucc/trabajoFinal/dao/PrioridadDao.java
@@ -0,0 +1,13 @@
+package ar.edu.ucc.trabajoFinal.dao;
+
+import ar.edu.ucc.trabajoFinal.model.Prioridad;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author ezequiel
+ */
+@Repository
+public class PrioridadDao  extends DaoGenericoImp<Prioridad, Long> implements IPrioridadDao{
+    
+}
